79e8efba01cdec955b45234ecb7bf11c1c71e1d2
Roma Voroneshski <romochka@gmail.com>
2022-08-13 09:54:51 +0300

    nolock

--- tokens/tests.js
+++ tokens/tests.js
@@ -1,7 +1,6 @@
-
-const d = {
-   color: {
-      raw: {
+const node = {
+   light: {
+      color: {
          coolGray: {
             value: "#8F9397",
             type: "color",
@@ -16,45 +15,5 @@ const d = {
             type: "color",
          },
       },
-      get coolGray() { return this.raw.coolGray.value },
-      get creamy() { return this.raw.creamy.value },
-      get greenishBlack() { return this.raw.greenishBlack.value },
    },
 };
-
-const fb = node => {
-   return node;
-};
-
-const f = node => {
-   const fbNode = {
-      source: node,
-   };
-   const keys = Object.keys(node);
-   keys.forEach(key => {
-      Object.defineProperty(fbNode, key, {
-         get() {
-            return fb(this.source[key]);
-         },
-      });
-   });
-   return fbNode;
-};
-
-// const fbNode = f(d);
-
-const handler = {
-   get(target, prop, receiver) {
-      console.log(`prop requested:`, prop);
-      return target[prop];
-   },
-};
-
-const proxy = new Proxy(d, handler);
-
-/* console.log(
-   `tree with getters:`,
-   util.inspect(fbNode, { getters: true, showHidden: true, depth: 100 })
-); */
-
-console.log(proxy.color.creamy);
